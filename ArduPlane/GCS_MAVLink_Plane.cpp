7ba9568e4d6dacc988cc61bdb14d1e4fb89fc8e1
martin <237minus@gmail.com>
2025-04-28 13:55:40 +0200

    fixed var

--- ArduPlane/GCS_MAVLink_Plane.cpp
+++ ArduPlane/GCS_MAVLink_Plane.cpp
@@ -1112,18 +1112,18 @@ void GCS_MAVLINK_Plane::handle_set_attitude_target(const mavlink_message_t &msg)
         if(1){
 
 
-            desired_rates.x = att_target.body_roll_rate;
+            float b_r_r = att_target.body_roll_rate;
         
-            desired_rates.y = att_target.body_pitch_rate;
+            float b_p_r = att_target.body_pitch_rate;
     
-            desired_rates.z = att_target.body_yaw_rate;
+            float b_y_r = att_target.body_yaw_rate;
 
             const float speed_scaler = plane.get_speed_scaler();
   
             // call to rate controllers
-            SRV_Channels::set_output_scaled(SRV_Channel::k_aileron,  plane.rollController.get_rate_out(desired_rates.x, speed_scaler));
-            SRV_Channels::set_output_scaled(SRV_Channel::k_elevator, plane.pitchController.get_rate_out(desired_rates.y, speed_scaler));
-            output_rudder_and_steering(plane.yawController.get_rate_out(desired_rates.z,  speed_scaler, false));
+            SRV_Channels::set_output_scaled(SRV_Channel::k_aileron,  plane.rollController.get_rate_out(b_r_r, speed_scaler));
+            SRV_Channels::set_output_scaled(SRV_Channel::k_elevator, plane.pitchController.get_rate_out(b_p_r, speed_scaler));
+            output_rudder_and_steering(plane.yawController.get_rate_out(b_y_r,  speed_scaler, false));
 
             plane.guided_state.forced_throttle = att_target.thrust * 100.0f;
             plane.guided_state.last_forced_throttle_ms = now;
