7d9c3115f7e35fcc9b3bb07101fbea733a69b33d
martin <237minus@gmail.com>
2025-04-29 12:08:36 +0200

    fix

--- ArduPlane/GCS_MAVLink_Plane.cpp
+++ ArduPlane/GCS_MAVLink_Plane.cpp
@@ -1118,13 +1118,14 @@ void GCS_MAVLINK_Plane::handle_set_attitude_target(const mavlink_message_t &msg)
         
             float b_p_r = att_target.body_pitch_rate;
     
-            //float b_y_r = att_target.body_yaw_rate;
+            float b_y_r = att_target.body_yaw_rate;
 
             const float speed_scaler = plane.get_speed_scaler();
   
             // call to rate controllers
             SRV_Channels::set_output_scaled(SRV_Channel::k_aileron,  plane.rollController.get_rate_out(b_r_r, speed_scaler));
             SRV_Channels::set_output_scaled(SRV_Channel::k_elevator, plane.pitchController.get_rate_out(b_p_r, speed_scaler));
+            output_rudder_and_steering(plane.yawController.get_rate_out(b_y_r,  speed_scaler, false));
             //SRV_Channels::set_output_scaled(SRV_Channel::k_rudder, plane.yawController.get_rate_out(b_y_r, speed_scaler));
 
             plane.guided_state.forced_throttle = att_target.thrust * 100.0f;
