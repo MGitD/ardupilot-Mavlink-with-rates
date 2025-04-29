0f763cc6b60d097cc0ac3684357de38b19e0d1f8
martin <237minus@gmail.com>
2025-04-29 08:27:25 +0200

    fix

--- ArduPlane/GCS_MAVLink_Plane.cpp
+++ ArduPlane/GCS_MAVLink_Plane.cpp
@@ -1109,6 +1109,7 @@ void GCS_MAVLINK_Plane::handle_set_attitude_target(const mavlink_message_t &msg)
         
         // Rate control
         //if(att_target.type_mask & 0b10111000 == 0b10111000):
+        uint32_t now = AP_HAL::millis();
         if(1){
 
 
@@ -1116,24 +1117,20 @@ void GCS_MAVLINK_Plane::handle_set_attitude_target(const mavlink_message_t &msg)
         
             float b_p_r = att_target.body_pitch_rate;
     
-            float b_y_r = att_target.body_yaw_rate;
+            //float b_y_r = att_target.body_yaw_rate;
 
             const float speed_scaler = plane.get_speed_scaler();
   
             // call to rate controllers
             //SRV_Channels::set_output_scaled(SRV_Channel::k_aileron,  plane.rollController.get_rate_out(b_r_r, speed_scaler));
             SRV_Channels::set_output_scaled(SRV_Channel::k_elevator, plane.pitchController.get_rate_out(b_p_r, speed_scaler));
-            SRV_Channels::set_output_scaled(SRV_Channel::k_rudder, plane.yawController.get_rate_out(b_y_r, speed_scaler));
+            //SRV_Channels::set_output_scaled(SRV_Channel::k_rudder, plane.yawController.get_rate_out(b_y_r, speed_scaler));
             //output_rudder_and_steering(plane.yawController.get_rate_out(b_y_r,  speed_scaler, false));
 
             plane.guided_state.forced_rpy_cd.x = degrees(q.get_euler_roll()) * 100.0f;
             plane.guided_state.forced_throttle = att_target.thrust * 100.0f;
             
-            //plane.guided_state.last_forced_throttle_ms = now;
-
-            //acro_state.roll_active_last = roll_active;
-            //acro_state.pitch_active_last = pitch_active;
-            //acro_state.yaw_active_last = yaw_active;
+            plane.guided_state.last_forced_throttle_ms = now;
         }
 
         else{
@@ -1144,7 +1141,6 @@ void GCS_MAVLINK_Plane::handle_set_attitude_target(const mavlink_message_t &msg)
 
             uint8_t attitude_mask = att_target.type_mask & 0b10000111; // q plus rpy
 
-            uint32_t now = AP_HAL::millis();
             if ((attitude_mask & 0b10000001) ||    // partial, including roll
                     (attitude_mask == 0b10000000)) { // all angles
                 plane.guided_state.forced_rpy_cd.x = degrees(q.get_euler_roll()) * 100.0f;
